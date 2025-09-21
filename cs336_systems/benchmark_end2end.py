d0842c05b096240075bf18bb799e503ed0420da5
zhangwbhi <zhangwbhi@gmail.com>
2025-09-21 15:20:35 -0400

    memory profiling added

--- cs336_systems/benchmark_end2end.py
+++ cs336_systems/benchmark_end2end.py
@@ -71,25 +71,42 @@ def benchmark_model_end_to_end(
     forward_times = []
     backward_times =[]
 
+    memory_before_bwd = []
+    memory_after_bwd = []
+
     with ctx:
-        for _ in range(repeats):
+        for i in range(repeats):
+            torch.cuda.reset_peak_memory_stats()
             start_time = timeit.default_timer()
+
             y_ = model(x)
             if device == torch.device("cuda") or device == "cuda":
                 torch.cuda.synchronize()
+            memory_before_bwd.append(torch.cuda.max_memory_allocated()/(1024**3))
             forward_times.append(timeit.default_timer() - start_time)
 
-            if not forward_only:
-                start_time = timeit.default_timer()
-                optimizer.zero_grad()
-                loss = cross_entropy(y_, x)
-                loss.backward()
-                optimizer.step()
-                if device == torch.device("cuda") or device == "cuda":
-                    torch.cuda.synchronize()
-                backward_times.append(timeit.default_timer() - start_time)
+            if forward_only:
+                memory_after_bwd.append(0.)
+                continue
 
-    return mean(forward_times), stdev(forward_times), mean(backward_times) if not forward_only else None, stdev(backward_times) if not forward_only else None
+            start_time = timeit.default_timer()
+            optimizer.zero_grad()
+            loss = cross_entropy(y_, x)
+            loss.backward()
+            optimizer.step()
+            if device == torch.device("cuda") or device == "cuda":
+                torch.cuda.synchronize()
+
+
+            memory_after_bwd.append(torch.cuda.max_memory_allocated()/(1024**3))
+            backward_times.append(timeit.default_timer() - start_time)
+
+
+    return mean(forward_times), stdev(forward_times), \
+        mean(backward_times) if not forward_only else None, \
+        stdev(backward_times) if not forward_only else None, \
+        mean(memory_before_bwd), stdev(memory_before_bwd), \
+        mean(memory_after_bwd), stdev(memory_after_bwd)
 
 
 
@@ -116,7 +133,8 @@ def main():
     for model_name, autocast in itertools.product(model_configs["models"], [True, False]):
         config = model_configs["models"][model_name]
         print(f"Benchmarking model: {model_name}")
-
+        torch.cuda.empty_cache()
+        torch.cuda.memory._record_memory_history(max_entries=10000)
         # Initialize model
         model = BasicsTransformerLM(
             vocab_size=shared_params["vocab_size"],
@@ -136,7 +154,9 @@ def main():
             shared_params["device"]
         )
 
-        fwd_mean, fwd_std, bwd_mean, bwd_std =benchmark_model_end_to_end(model, x, shared_params["forward_only"], shared_params["warmup_steps"], shared_params["repeats"], shared_params["device"], autocast)
+        fwd_mean, fwd_std, bwd_mean, bwd_std, mem_before_bwd_mean, mem_before_bwd_std, mem_after_bwd_mean, mem_after_bwd_std =benchmark_model_end_to_end(model, x, shared_params["forward_only"], shared_params["warmup_steps"], shared_params["repeats"], shared_params["device"], autocast)
+        torch.cuda.memory._dump_snapshot(f"memory_snapshot_{"autocast" if autocast else "no_autocast"}_{"fwd_only" if shared_params["forward_only"] else "fwd_bwd"}.pickle")
+        torch.cuda.memory._record_memory_history(enabled=None)
         del model, x
         torch.cuda.empty_cache()
 
@@ -151,6 +171,9 @@ def main():
             "Std fwd (s)": round(fwd_std, 6),
             "Mean time bwd (s)": round(bwd_mean, 6) if not shared_params["forward_only"] else None,
             "Std bwd (s)": round(bwd_std, 6) if not shared_params["forward_only"] else None,
+            "Memory before bwd (GB)": mem_before_bwd_mean,
+            "Memory after bwd (GB)": mem_after_bwd_mean,
+
             "Warmup Steps": shared_params["warmup_steps"],
             "autocast": autocast
         })
